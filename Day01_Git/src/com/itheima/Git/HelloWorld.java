7c5225e7f428bc15a1210cc6cc16ab1390a3021e
heimaTest <heimaTest@itcast.cn>
2020-11-13 17:59:19 +0800

    这是关于dev分支的测试

--- Day01_Git/src/com/itheima/Git/HelloWorld.java
+++ Day01_Git/src/com/itheima/Git/HelloWorld.java
@@ -3,5 +3,6 @@ package com.itheima.Git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("这是一次Git测试");
+        System.out.println("这是关于dev分支的测试");
     }
 }
