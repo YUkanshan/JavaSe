2809bd87b00af36cf74fb19cf1e1a5a524819e22
余看山 <8308100+yu-kan-shan@user.noreply.gitee.com>
2020-11-13 20:17:22 +0800

    update Day01_Git/src/com/itheima/Git/Test01.java.

--- Day01_Git/src/com/itheima/Git/Test01.java
+++ Day01_Git/src/com/itheima/Git/Test01.java
@@ -3,5 +3,9 @@ package com.itheima.Git;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("这是一次关于远程仓库传输的测试");
-    }
+    System.out.println("这是关于从码云克隆的测试");
+        
+            
+                 
+                     }
 }
