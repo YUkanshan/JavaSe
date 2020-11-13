799d559d32dda47d0a2f4e409e03f8323c3de307
heimaTest <heimaTest@itcast.cn>
2020-11-13 21:32:14 +0800

    先push再pull就可以提示错误 先pull再push就可以先修正不出现错误

--- Day01_Git/src/com/itheima/Git/Test01.java
+++ Day01_Git/src/com/itheima/Git/Test01.java
@@ -6,7 +6,9 @@ public class Test01 {
     System.out.println("这是关于从码云克隆的测试");
 
 int i=20;
-
+//先push再pull就可以出现提示错误
+        //然后修改完以后push就可以将最终版本推上去
+        //如果想依照仓库版本来编写的话,就先进行pull(修正)再push
                  
                      }
 }
